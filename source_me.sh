e69d57de4ff9f242e5a825ddb4d5e1bd0a9278a2
Bassam Khouri <bassam.khouri@gmail.com>
2020-10-30 09:15:24 -0400

    add source_me.sh

---
+++ source_me.sh
@@ -0,0 +1,7 @@
+#export PATH="/usr/local/opt/openjdk@11/bin:${PATH}"
+#export CPPFLAGS="-I/usr/local/opt/openjdk@11/include"
+#export JAVA_HOME="/usr/local/opt/openjdk@11"
+
+export PATH="/usr/local/opt/openjdk@8/bin:${PATH}"
+export CPPFLAGS="-I/usr/local/opt/openjdk@8/include"
+export JAVA_HOME="/usr/local/opt/openjdk@8"
